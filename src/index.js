f92db3a4c5aa5743d905e2522fe6209b3770ea2b
moses mukabi <mosesmukabi9@gmai.com>
2024-11-22 09:18:06 +0300

    implemented the  login route

--- src/index.js
+++ src/index.js
@@ -9,7 +9,8 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cors({
     origin:["http://localhost:5173"],
-    methods:["GET", "POST", "PATCH", "PUT", "DELETE"]
+    methods:["GET", "POST", "PATCH", "PUT", "DELETE"],
+    credentials: true,
 
 }))
 
